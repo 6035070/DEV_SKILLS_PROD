93c144d2ce3fddce0be6cdac7dce0c4ea92e0a85
Luke-technolab <luke@technolableiden.nl>
2026-02-04 11:11:02 +0100

    test

---
+++ Test_Omgeving/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Aanvraag nieuwsbrief</h1>
+
+<form method="post">
+    <input type="text" name="Voornaam" placeholder="Voornaam" required>
+    <input type="text" name="Tussenvoegsel" placeholder="TussenVoegsel" >
+    <input type="text" name="Achternaam" placeholder="Achternaam" required>
+    <input type="text" name="Straat" placeholder="Straatnaam en nummer" required>
+    <input type="email" name="email" placeholder="E-mail" required>
+    <input type="text" name="Postcode" placeholder="Postcode" required>
+    <input type="text" name="Woonplaats" placeholder="Woonplaats" required>
+    <input type="tel" name="Telefoon" placeholder="Telefoon nummer">
+
+    <input type="submit" value="Verstuur">
+</form>
+    
+</body>
+</html>
